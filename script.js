c5819b60c044edfcf7b9533907e590435f2e864d
Mohammad Farmaan <farmaanghouse6@gmail.com>
2020-07-08 12:44:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Clears the screen on click of C button.
+function clearScreen() {
+  document.getElementById("result").value = "";
+}
+// Displays entered value on screen.
+function liveScreen(value) {
+  document.getElementById("result").value += value;
+}
+// Swaps the style sheet in order to  achieve dark mode.
+function switchTheme() {
+  let darkMode = document.getElementById("dark-mode");
+  let theme = document.getElementById("theme");
+  if (theme.getAttribute("href") == "light.css") {
+    theme.href = "dark.css";
+    darkMode.innerHTML = "Light Mode 🌙";
+  } else {
+    theme.href = "light.css";
+    darkMode.innerHTML = "Dark Mode 🌙";
+  }
+}
