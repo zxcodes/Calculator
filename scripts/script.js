d1d06a650068ca62377273560688dc853233c867
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2022-04-10 08:01:32 +0530

    feat:
    :sparkles: clearEntryScreen() function was added to remove the last input using CE button. :bug: textarea extra space in the bottom problem was resolved using vertical-align: top;

--- scripts/script.js
+++ scripts/script.js
@@ -2,92 +2,128 @@ let lightTheme = "styles/light.css";
 let darkTheme = "styles/dark.css";
 
 //adding event handler on the document to handle keyboard inputs
-document.addEventListener("keydown", keyboardInputHandler);
+document.addEventListener( "keydown", keyboardInputHandler );
 
 //function to handle keyboard inputs
-function keyboardInputHandler(e) {
+function keyboardInputHandler ( e )
+{
   //grabbing the liveScreen
-  let res = document.getElementById("input");
-  let result = document.getElementById("result");
+  let res = document.getElementById( "input" );
+  let result = document.getElementById( "result" );
 
   //numbers
-  if (e.key === "0") {
+  if ( e.key === "0" )
+  {
     res.value += "0";
-  } else if (e.key === "1") {
+  } else if ( e.key === "1" )
+  {
     res.value += "1";
-  } else if (e.key === "2") {
+  } else if ( e.key === "2" )
+  {
     res.value += "2";
-  } else if (e.key === "3") {
+  } else if ( e.key === "3" )
+  {
     res.value += "3";
-  } else if (e.key === "4") {
+  } else if ( e.key === "4" )
+  {
     res.value += "4";
-  } else if (e.key === "5") {
+  } else if ( e.key === "5" )
+  {
     res.value += "5";
-  } else if (e.key === "6") {
+  } else if ( e.key === "6" )
+  {
     res.value += "6";
-  } else if (e.key === "7") {
+  } else if ( e.key === "7" )
+  {
     res.value += "7";
-  } else if (e.key === "7") {
+  } else if ( e.key === "7" )
+  {
     res.value += "7";
-  } else if (e.key === "8") {
+  } else if ( e.key === "8" )
+  {
     res.value += "8";
-  } else if (e.key === "9") {
+  } else if ( e.key === "9" )
+  {
     res.value += "9";
   }
 
   //operators
-  if (e.key === "+") {
+  if ( e.key === "+" )
+  {
     res.value += "+";
-  } else if (e.key === "-") {
+  } else if ( e.key === "-" )
+  {
     res.value += "-";
-  } else if (e.key === "*") {
+  } else if ( e.key === "*" )
+  {
     res.value += "*";
-  } else if (e.key === "/") {
+  } else if ( e.key === "/" )
+  {
     res.value += "/";
   }
 
   //decimal key
-  if (e.key === ".") {
+  if ( e.key === "." )
+  {
     res.value += ".";
   }
 
   //press enter to see result
-  if (e.key === "Enter") {
+  if ( e.key === "Enter" )
+  {
     //result.value = eval(result.value || null);
-    result.value = eval(result.value || null);
-    result = eval(result.value || null);
-    console.log(result);
+    result.value = eval( result.value || null );
+    result = eval( result.value || null );
+    console.log( result );
   }
 
   //backspace for removing the last input
-  if (e.key === "Backspace") {
+  if ( e.key === "Backspace" )
+  {
     let resultInput = res.value;
 
     //remove the last element in the string
-    res.value = resultInput.substring(0, res.value.length - 1);
+    res.value = resultInput.substring( 0, res.value.length - 1 );
   }
 }
 
 // Clears the screen on click of C button.
-function clearScreen() {
-  document.getElementById("input").value = "";
+function clearScreen ()
+{
+  document.getElementById( "input" ).value = "";
+}
+
+
+//remove the last element in the string using button
+function clearEntryScreen ()
+{
+  let res = document.getElementById( "input" );
+  let resultInput = res.value;
+  res.value = resultInput.substring( 0, res.value.length - 1 );
 }
 // Displays entered value on screen.
-function liveScreen(value) {
-  let res = document.getElementById("input");
-  if (!res.value) {
+function liveScreen ( value )
+{
+  let res = document.getElementById( "input" );
+  if ( !res.value )
+  {
     res.value = "";
   }
   res.value += value;
 }
+
+
 // Swaps the stylesheet in order to  achieve dark mode.
-function changeTheme() {
-  let darkMode = document.getElementById("dark-mode");
-  let theme = document.getElementById("theme");
-  if (theme.getAttribute("href") === lightTheme) {
+function changeTheme ()
+{
+  let darkMode = document.getElementById( "dark-mode" );
+  let theme = document.getElementById( "theme" );
+  if ( theme.getAttribute( "href" ) === lightTheme )
+  {
     theme.href = darkTheme;
     darkMode.innerHTML = "Light Mode 🌞";
-  } else {
+  } else
+  {
     theme.href = lightTheme;
     darkMode.innerHTML = "Dark Mode 🌙";
   }
