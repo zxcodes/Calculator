e18da05e58c30486b9ccb983dd4311aa29e8cecb
Mohammed Farmaan <farmaanghouse6@gmail.com>
2022-05-19 13:35:13 +0530

    Minor updates

--- scripts/script.js
+++ scripts/script.js
@@ -27,11 +27,11 @@ function changeTheme() {
   if (theme.getAttribute("href") === lightTheme) {
     theme.setAttribute("href", darkTheme);
     themeIcon.setAttribute("src", sunIcon);
-    toast.innerHTML = "Dark Mode";
+    toast.innerHTML = "Dark Mode 🌙";
   } else {
     theme.setAttribute("href", lightTheme);
     themeIcon.setAttribute("src", moonIcon);
-    toast.innerHTML = "Light Mode";
+    toast.innerHTML = "Light Mode ☀️";
   }
 }
 
